eb7c60801eeabc5b42a97c94c6898108d0101aac
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2020-04-19 13:49:41 -0400

    finished time converter

--- js/time.js
+++ js/time.js
@@ -1,205 +1,239 @@
 function convert() {
-  var first = document.getElementById("firstInput").value
-  var second = document.getElementById("secondInput").value
-  var firstDropdown = document.getElementById("firstDropdown").value
-  var secondDropdown = document.getElementById("secondDropdown").value
+  var first = document.getElementById("firstInput")
+  var second = document.getElementById("secondInput")
+  var firstDropdown = document.getElementById("firstDropdown")
+  var secondDropdown = document.getElementById("secondDropdown")
 
-  if (first === null) {
-    break;
+  if (first.value === null) {
+    alert("Please enter a value in the first input box.")
   } else {
 
-    switch (firstDropdown) {
+    switch (Number(firstDropdown.value)) {
       case 1:
-          switch (secondDropdown) {
+          switch (Number(secondDropdown.value)) {
             case 1:
-               second = first
+               second.value = first.value
                second.toLocaleString('en')
             break;
             case 2:
-               second = first * 12
+               second.value = first.value * 12
                second.toLocaleString('en')
             break;
             case 3:
-               second = first * 52
+               second.value = first.value * 52
                second.toLocaleString('en')
             break;
             case 4:
-               second = first * 365
+               second.value = first.value * 365
                second.toLocaleString('en')
             break;
             case 5:
-               second = first * 8760
+               second.value = first.value * 8760
                second.toLocaleString('en')
             break;
             case 6:
-               second = first * 525600
+               second.value = first.value * 525600
                second.toLocaleString('en')
             break;
             case 7:
-               second = first * 31540000
+               second.value = first.value * 31540000
                second.toLocaleString('en')
             break;
           }
 
       break;
       case 2:
-          switch (secondDropdown) {
+          switch (Number(secondDropdown.value)) {
             case 1:
-              second = first / 12
+              second.value = first.value / 12
               second.toLocaleString('en')
             break;
             case 2:
-              second = first
+              second.value = first
               second.toLocaleString('en')
             break;
             case 3:
-              second = first * 4
+              second.value = first.value * 4
               second.toLocaleString('en')
             break;
             case 4:
-              second = first * 30
+              second.value = first.value * 30
               second.toLocaleString('en')
             break;
             case 5:
-              second = first * 730
+              second.value = first.value * 730
               second.toLocaleString('en')
             break;
             case 6:
-              second = first * 43800
+              second.value = first.value * 43800
               second.toLocaleString('en')
             break;
             case 7:
-              second = first * 2628000
+              second.value = first.value * 2628000
               second.toLocaleString('en')
             break;
           }
       break;
       case 3:
-          switch (secondDropdown) {
+          switch (Number(secondDropdown.value)) {
             case 1:
-              second = first / 52
+              second.value = first.value / 52
               second.toLocaleString('en')
             break;
             case 2:
-              second = first / 4
+              second.value = first.value / 4
               second.toLocaleString('en')
             break;
             case 3:
-              second = first
+              second.value = first
               second.toLocaleString('en')
             break;
             case 4:
-              second = first * 7
+              second.value = first.value * 7
               second.toLocaleString('en')
             break;
             case 5:
-              second = first * 168
+              second.value = first.value * 168
               second.toLocaleString('en')
             break;
             case 6:
-              second = first * 10080
+              second.value = first.value * 10080
               second.toLocaleString('en')
             break;
             case 7:
-              second = first * 604800
+              second.value = first.value * 604800
               second.toLocaleString('en')
             break;
           }
       break;
       case 4:
-          switch (secondDropdown) {
+          switch (Number(secondDropdown.value)) {
             case 1:
-              second = first / 365
+              second.value = first.value / 365
               second.toLocaleString('en')
             break;
             case 2:
-              second = first / 30
+              second.value = first.value / 30
               second.toLocaleString('en')
             break;
             case 3:
-              second = first / 7
+              second.value = first.value / 7
               second.toLocaleString('en')
             break;
             case 4:
-              second = first
+              second.value = first
               second.toLocaleString('en')
             break;
             case 5:
-              second = first * 24
+              second.value = first.value * 24
               second.toLocaleString('en')
             break;
             case 6:
-              second = first * 1440
+              second.value = first.value * 1440
               second.toLocaleString('en')
             break;
             case 7:
-              second = first * 86400
+              second.value = first.value * 86400
               second.toLocaleString('en')
             break;
           }
       break;
       case 5:
-          switch (secondDropdown) {
+          switch (Number(secondDropdown.value)) {
             case 1:
-              second = first / 8760
+              second.value = first.value / 8760
               second.toLocaleString('en')
             break;
             case 2:
-              second = first / 730
+              second.value = first.value / 730
               second.toLocaleString('en')
             break;
             case 3:
-              second = first / 168
+              second.value = first.value / 168
               second.toLocaleString('en')
             break;
             case 4:
-              second = first / 24
+              second.value = first.value / 24
               second.toLocaleString('en')
             break;
             case 5:
-
+              second.value = first
+              second.toLocaleString('en')
+            break;
             case 6:
-
+              second.value = first.value * 60
+              second.toLocaleString('en')
+            break;
             case 7:
+              second.value = first.value * 3600
+              second.toLocaleString('en')
+            break;
           }
       break;
       case 6:
-          switch (secondDropdown) {
+          switch (Number(secondDropdown.value)) {
             case 1:
-
+              second.value = first.value / 525600
+              second.toLocaleString('en')
+            break;
             case 2:
-
+              second.value = first.value / 43800
+              second.toLocaleString('en')
+            break;
             case 3:
-
+              second.value = first.value / 10080
+              second.toLocaleString('en')
+            break;
             case 4:
-
+              second.value = first.value / 1440
+              second.toLocaleString('en')
+            break;
             case 5:
-
+              second.value = first.value / 60
+              second.toLocaleString('en')
+            break;
             case 6:
-
+              second.value = first
+              second.toLocaleString('en')
+            break;
             case 7:
+              second.value = first.value * 60
+              second.toLocaleString('en')
+            break;
           }
       break;
       case 7:
-          switch (secondDropdown) {
+          switch (Number(secondDropdown.value)) {
             case 1:
-
+              second.value = first.value / 31540000
+              second.toLocaleString('en')
+            break;
             case 2:
-
+              second.value = first.value / 2628000
+              second.toLocaleString('en')
+            break;
             case 3:
-
+              second.value = first.value / 604800
+              second.toLocaleString('en')
+            break;
             case 4:
-
+              second.value = first.value / 86400
+              second.toLocaleString('en')
+            break;
             case 5:
-
+              second.value = first.value / 3600
+              second.toLocaleString('en')
+            break;
             case 6:
-
+              second.value = first.value / 60
+              second.toLocaleString('en')
+            break;
             case 7:
+              second.value = first
+              second.toLocaleString('en')
+            break;
           }
       break;
     }
-
-
   }
-
 }
