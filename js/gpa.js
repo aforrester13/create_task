2fe50a03a5e1271be0f5b1d00d69d660a0621850
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2020-04-22 15:30:25 -0400

    fix add button

--- js/gpa.js
+++ js/gpa.js
@@ -1,8 +1,10 @@
+// Variables
+let counter = 8;
+var grades = []
+var hours = []
 // Functions
 
 function gpa() {
-  let grades = []
-  let hours = []
   let grade1 = document.getElementById("grade1").value
   let hour1 = document.getElementById("hour1").value
   let grade2 = document.getElementById("grade2").value
@@ -20,30 +22,26 @@ function gpa() {
   let grade8 = document.getElementById("grade8").value
   let hour8 = document.getElementById("hour8").value
   let calculate = document.getElementById("calculate")
+  let addBtn = document.getElementById("addClass")
 
 
   var inputs = document.getElementsByClassName("input");
   var sum = 0;
   var hourSum = 0;
 
+  if (addBtn.clicked === true) {
+    counter = counter + 1
+  }
+
   grades.push(grade1, grade2, grade3, grade4, grade5, grade6, grade7, grade8)
   hours.push(hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8)
 
-  
-
-  for (let x = 0; x < 16; x++) {
-    if (inputs[x].value == null) {
-      break;
-    } else {
-      continue;
-    }
-  }
-
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < Number(counter); i++) {
     sum = sum + (grades[i] * hours[i])
   }
 
-  for (let j = 0; j < 8; j++) {
+  console.log(sum)
+  for (let j = 0; j < Number(counter); j++) {
     hourSum = hourSum + Number(hours[j])
   }
 
@@ -53,4 +51,90 @@ function gpa() {
   var lastInput = document.getElementById("final")
 
   lastInput.value = final;
+}
+
+function add() {
+  counter = counter + 1;
+
+  var table = document.getElementById("table");
+  var tr = table.insertRow(counter)
+
+  var td1 = tr.insertCell(0);
+  td1.id = "td1"
+  td1.innerHTML = "Class " + String(counter);
+  Number(counter);
+
+  var td2 = tr.insertCell(1);
+  td2.id = "td2";
+  var select = document.createElement("SELECT");
+  select.id = "nextGrade";
+
+    var a = document.createElement("OPTION");
+    a.value = 4.0;
+    a.text = "A";
+    select.add(a);
+
+    var aMinus = document.createElement("OPTION");
+    aMinus.value = 3.7;
+    aMinus.text = "A-";
+    select.add(aMinus);
+
+    var bPlus = document.createElement("OPTION");
+    bPlus.value = 3.3;
+    bPlus.text = "B+";
+    select.add(bPlus);
+
+    var b = document.createElement("OPTION");
+    b.value = 3.0;
+    b.text = "B";
+    select.add(b);
+
+    var bMinus = document.createElement("OPTION");
+    bMinus.value = 2.7;
+    bMinus.text = "B-";
+    select.add(bMinus);
+
+    var cPlus = document.createElement("OPTION");
+    cPlus.value = 2.3;
+    cPlus.text = "C+";
+    select.add(cPlus);
+
+    var c = document.createElement("OPTION");
+    c.value = 2.0;
+    c.text = "C";
+    select.add(c);
+
+    var cMinus = document.createElement("OPTION");
+    cMinus.value = 1.7;
+    cMinus.text = "C-";
+    select.add(cMinus);
+
+    var dPlus = document.createElement("OPTION");
+    dPlus.value = 1.3;
+    dPlus.text = "D+";
+    select.add(dPlus);
+
+    var d = document.createElement("OPTION");
+    d.value = 1.0;
+    d.text = "D";
+    select.add(d);
+
+    var f = document.createElement("OPTION");
+    f.value = 0.0;
+    f.text = "F";
+    select.add(f);
+
+
+  td2.append(select)
+  grades.push(document.getElementById("nextGrade").value)
+
+  var td3 = tr.insertCell(2);
+  td3.id = "td3";
+  var input = document.createElement("INPUT")
+  input.type = "text";
+  input.class = "input";
+  input.id = "nextHour"
+  td3.append(input);
+  hours.push(document.getElementById("nextHour").value)
+
 }
