26bbf9bcea6add6bbea634cd4043cca30e93eeb0
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2020-04-23 16:01:05 -0400

    gave up on add button

--- js/gpa.js
+++ js/gpa.js
@@ -1,151 +1,11 @@
 // Variables
-var counter = 8;
-var grades = [];
-var hours = []
-var sum = 0;
-var hourSum = 0;
-var select;
-// Functions
-function add() {
-  counter++
-
-  let hiddenInput = document.getElementById("hidden");
-  hiddenInput.value = counter
-
-  var table = document.getElementById("table");
-  var tr = table.insertRow(counter)
-
-  var td1 = tr.insertCell(0);
-  td1.id = "td1"
-  td1.innerHTML = "Class " + String(counter);
-  Number(counter);
-
-  var td2 = tr.insertCell(1);
-  td2.id = "td2";
-  select = document.createElement("SELECT");
-  select.id = "nextGrade";
-
-    var a = document.createElement("OPTION");
-    a.value = 4.0;
-    a.text = "A";
-    select.add(a);
-
-    var aMinus = document.createElement("OPTION");
-    aMinus.value = 3.7;
-    aMinus.text = "A-";
-    select.add(aMinus);
-
-    var bPlus = document.createElement("OPTION");
-    bPlus.value = 3.3;
-    bPlus.text = "B+";
-    select.add(bPlus);
-
-    var b = document.createElement("OPTION");
-    b.value = 3.0;
-    b.text = "B";
-    select.add(b);
-
-    var bMinus = document.createElement("OPTION");
-    bMinus.value = 2.7;
-    bMinus.text = "B-";
-    select.add(bMinus);
-
-    var cPlus = document.createElement("OPTION");
-    cPlus.value = 2.3;
-    cPlus.text = "C+";
-    select.add(cPlus);
-
-    var c = document.createElement("OPTION");
-    c.value = 2.0;
-    c.text = "C";
-    select.add(c);
-
-    var cMinus = document.createElement("OPTION");
-    cMinus.value = 1.7;
-    cMinus.text = "C-";
-    select.add(cMinus);
-
-    var dPlus = document.createElement("OPTION");
-    dPlus.value = 1.3;
-    dPlus.text = "D+";
-    select.add(dPlus);
-
-    var d = document.createElement("OPTION");
-    d.value = 1.0;
-    d.text = "D";
-    select.add(d);
-
-    var f = document.createElement("OPTION");
-    f.value = 0.0;
-    f.text = "F";
-    select.add(f);
-  td2.append(select)
-
-  var selectText = select.textContent
-
-
-  var td3 = tr.insertCell(2);
-  td3.id = "td3";
-  var input = document.createElement("INPUT")
-  input.type = "text";
-  input.id = "nextHour";
-  td3.append(input);
-
- grades.push(select.value)
-
-   /* switch (String(selectText)) {
-    case "A":
-    grades.push(a.value)
-    break;
-
-    case "A-":
-    grades.push(aMinus.value)
-    break;
-
-    case "B+":
-    grades.push(bPlus.value)
-    break;
-
-    case "B":
-    grades.push(b.value)
-    break;
-
-    case "B-":
-    grades.push(bMinus.value)
-    break;
-
-    case "C+":
-    grades.push(cPlus.value)
-    break;
-
-    case "C":
-    grades.push(c.value)
-    break;
-
-    case "C-":
-    grades.push(cMinus.value)
-    break;
-
-    case "D+":
-    grades.push(dPlus.value)
-    break;
-
-    case "D":
-    grades.push(d.value)
-    break;
-
-    case "F":
-    grades.push(f.value)
-    break;
-  }
-
-  hours.push(input.value)
-*/
-
-
-
-}
-
+let counter = 8;
+let grades = [];
+let hours = []
+let sum = 0;
+let hourSum = 0;
+let lastInput;
+let final;
 
 
 function gpa() {
@@ -168,29 +28,34 @@ function gpa() {
   let calculate = document.getElementById("calculate");
   let addBtn = document.getElementById("addClass");
 
-  let secondCounter = document.getElementById("hidden").value;
+  grades.push(grade1, grade2, grade3, grade4, grade5, grade6, grade7, grade8)
+  hours.push(hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8)
 
-
-
-  grades.unshift(grade1, grade2, grade3, grade4, grade5, grade6, grade7, grade8)
-  hours.unshift(hour1, hour2, hour3, hour4, hour5, hour6, hour7, hour8)
-
-  console.log(grades)
-
-  for (let i = 0; i < secondCounter; i++) {
+  for (let i = 0; i < 8; i++) {
     sum = sum + (grades[i] * hours[i])
   }
 
-  // console.log(sum)
-
-  for (let j = 0; j < secondCounter; j++) {
+  for (let j = 0; j < 8; j++) {
     hourSum = hourSum + Number(hours[j])
   }
 
-  var final = Number(sum/hourSum)
+  final = Number(sum/hourSum)
   final = (Math.floor(final * 100) / 100)
 
-  var lastInput = document.getElementById("final")
+  lastInput = document.getElementById("final")
 
-  lastInput.value = final;
+  if (Number.isNaN(final)) {
+    lastInput.value = "";
+  } else {
+    lastInput.value = "Your GPA is: " + String(final);
+  }
+
+  for (let y = 0; y < 8; y++) {
+    grades.pop(grades[y]);
+  }
+
+
+  for (let z = 0; z < 8; z++) {
+    hours.pop(hours[z]);
+  }
 }
